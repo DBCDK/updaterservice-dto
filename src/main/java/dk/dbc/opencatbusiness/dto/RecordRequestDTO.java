3e110efdc7b188ac3964945af51087f4529e1e52
Anders Thrane Michelsen <atm@dbc.dk>
2020-07-09 15:20:19 +0200

    Create RecordRequestDTO.java

---
+++ src/main/java/dk/dbc/opencatbusiness/dto/RecordRequestDTO.java
@@ -0,0 +1,35 @@
+package dk.dbc.opencatbusiness.dto;
+
+import java.util.Objects;
+
+public class RecordRequestDTO {
+    String record;
+
+    public String getRecord() {
+        return record;
+    }
+
+    public void setRecord(String record) {
+        this.record = record;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RecordRequestDTO that = (RecordRequestDTO) o;
+        return Objects.equals(record, that.record);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(record);
+    }
+
+    @Override
+    public String toString() {
+        return "RecordRequestDTO{" +
+                "record='" + record + '\'' +
+                '}';
+    }
+}
